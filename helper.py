24ed12444c478e416946fffacf9533463c2d3548
Ben <bkicken@gmail.com>
2024-05-26 01:23:11 +0200

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 tekst="header"
 lengte = len(tekst) + 4
 print()
@@ -5,3 +6,12 @@ print(lengte * "*")
 print(f"* {tekst} *")
 print(lengte * "*")
 print()
+=======
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
+>>>>>>> ec523c1 (functies toegevoegd)
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # 7.1.9. elementen met meer dan 5 of meer karakters worden in hoordletters geprint 
 prijzen = {
     "aardbei" : "3",
@@ -19,3 +20,33 @@ for el in reclame_tekst4:
         print(el.upper())
     else:
         print(el.lower())
+=======
+# importeer het helper.py bestand
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "appel": 3,
+        "banaan": 4,
+        "kers": 5
+        }
+    # 
+    aanbieding = prijzen["appel"] * 0.8   # 10% korting
+    reclame_tekst = f"Vandaag in de aanbieding: appel, per kilo - slechts € {aanbieding}"
+
+    # 
+    reclame_tekst2 = reclame_tekst[:58]
+
+    reclame_tekst3 = reclame_tekst2.upper()
+
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+decoreer("Aanbieding")
+print_aanbieding()
+
+>>>>>>> ec523c1 (functies toegevoegd)
