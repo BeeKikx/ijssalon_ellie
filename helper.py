7be75b0faf46867bf4b38bf15530f5b085e87ceb
Ben290462 <168919314+Ben290462@users.noreply.github.com>
2024-05-26 03:17:57 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
-=======
 def decoreer(tekst=""):
     lengte = len(tekst) + 4
     print()
