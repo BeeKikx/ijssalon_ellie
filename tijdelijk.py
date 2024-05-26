ef4f40c464194ffb313472e709dfaf1bddbb292f
Ben290462 <168919314+Ben290462@users.noreply.github.com>
2024-05-26 03:17:29 +0200

    Update tijdelijk.py

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-# 7.1.9. elementen met meer dan 5 of meer karakters worden in hoordletters geprint 
-prijzen = {
-    "aardbei" : "3",
-    "vanille" : "4",
-    "chocolade" : "5"
-    }
-
-aanbieding = prijzen["vanille"] * 1   # 10% korting
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
-
-reclame_tekst2 = reclame_tekst[:60]
-
-reclame_tekst3 = reclame_tekst.upper()
-
-reclame_tekst4 = reclame_tekst3.split()
-
-for el in reclame_tekst4:
-    if len(el) > 4:
-        print(el.upper())
-    else:
-        print(el.lower())
-=======
 # importeer het helper.py bestand
 from helper import decoreer
 
